12bb64b56e7dfa9265bbbbe8a0fe1f444826b41b
Saar A. Cohen <saohen92@outlook.com>
2021-08-02 17:57:46 +0300

    Uber Clone Project Creation Point

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+
+// Setup Redux
 
 export default function App() {
   return (
+    // <Provider>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Lets build Uber!</Text>
       <StatusBar style="auto" />
     </View>
+    // </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
